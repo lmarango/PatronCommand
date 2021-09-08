d2c0f1dd88c0a8a3a80ef1b1a5478bd67285f854
User <User@DESKTOP-ARTT035>
2021-09-08 12:22:27 -0500

    creada e implementada comando borrar

---
+++ src/main/java/co/edu/unicauca/commandrestaurant/domain/DeleteCommand.java
@@ -0,0 +1,59 @@
+package co.edu.unicauca.commandrestaurant.domain;
+
+import co.edu.unicauca.commandrestaurant.domain.service.FoodService;
+import co.edu.unicauca.commandrestaurant.access.RepositoryFactory;
+import co.edu.unicauca.commandrestaurant.access.IFoodRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Luis Arango
+ */
+public class DeleteCommand extends Command{
+    /**
+    * Comida a ser eliminada
+    */
+    private Food food;
+    /**
+    * Comida previa, que permitirá deshacer la operación de eliminar
+    */
+    private Food foodPrevious;
+    /**
+    * Instancia del receptor
+    */
+    private FoodService service;
+
+    /**
+     * Constructor parametrizado
+     * @param food comida a eliminar
+     */
+    public DeleteCommand(Food food) {
+        this.food = food;
+        IFoodRepository repo = RepositoryFactory.getInstance().getRepository("default");
+        service = new FoodService(repo);
+        this.hasUndo= true;
+
+    }
+
+    @Override
+    public void execute() {
+        Logger logger= LoggerFactory.getLogger(DeleteCommand.class); 
+        logger.info("Comando de eliminar ejecutado. Se eliminó la comida " + food.toString());
+        service.delete(food.getId());
+    }
+
+    @Override
+    public void undo() {
+        Logger logger= LoggerFactory.getLogger(DeleteCommand.class); 
+        logger.info("Comando de eliminar deshecho. Se recuperó la comida " + food.toString());
+        service.create(foodPrevious);
+    }
+     public Food getFoodPrevious() {
+        return foodPrevious;
+    }
+
+    public void setFoodPrevious(Food componentPrevious) {
+        this.foodPrevious = componentPrevious;
+    }
+}
